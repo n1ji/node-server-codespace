8a2a2aae24adb20566986a3244ecdb1881b50cfc
Myra <0.maysin.0@gmail.com>
2025-05-20 19:30:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,25 @@ const port = 3000;
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send(`
-	    <h1>Here's my home page</h1>
-      <p>Welcome to Express!</p>
-  `);
-
+app.get('/', (req, res) => {
+  res.send(`
+    <div style="text-align: center;">
+      <h1>Welcome to my home page!</h1>
+      <p>Nothing is here yet.</p>
+    </div>
+    `);
 });
 
-app.get('/about',(req,res)=>{
-    res.send(`
-	    <h1>Here's Bill's Express About Page</h1>
-      <p>Welcome to Express!</p>
-  `);
-
+app.get('/about', (req, res) => {
+  res.send(`
+    <div style="text-align: center;">
+      <h1>Welcome to Myra's About Page</h1>
+      <p>Myra is a YearUp student interning at Reddit.</p>
+    </div>
+    `);
 });
-
 
 //Set server to listen for requests
 app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
+  console.log(`Server running at port: ${port}/`);
 });
